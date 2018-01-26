d3cbb2924cb4ea22dd05752f10b6bdf82ec04cf5
gdifference <jp@grupodifference.com>
2018-01-26 18:20:43 -0200

    #1

---
+++ application/config/aws.php
@@ -0,0 +1,5 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+ 
+$config['aws_key']      = 'AKIAJSSZ5KVFHVZYKYDA';//root
+$config['aws_secret']   = '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';
